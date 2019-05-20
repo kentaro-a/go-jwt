8e3a807c7b7726b28fbc94efc58bebc81c46b39d
kentaro-a <minminzemi1121@yahoo.co.jp>
2019-05-20 19:04:36 +0900

    F

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,7 +1,12 @@
 package jwt
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
+	_ "fmt"
+	"strings"
 	"time"
 )
 
@@ -19,33 +24,107 @@ type JwtPayload struct {
 	Data     interface{} `json:"data"`
 }
 
-func Publish(alg string, data interface{}) (*Jwt, error) {
-	jwt = &Jwt{
-		JwtHeader{
+// const JwtExpiredTime time.Duration = time.Minute * 60 * 24	// 1day
+const JwtExpiredTime time.Duration = time.Minute * 1
+const Alg string = "HS256"
+
+func Publish(data interface{}) (*Jwt, error) {
+	jwt := &Jwt{
+		Header: JwtHeader{
 			"JWT",
-			alg,
+			Alg,
 		},
-		JwtPayload{
-			time.Now(),
+		Payload: JwtPayload{
+			time.Now().Add(JwtExpiredTime),
 			data,
 		},
 	}
-	jwt.makeSignature()
-	return jwt
+	err := jwt.setSignature()
+	return jwt, err
 }
 
-func (jwt *Jwt) makeSignature() {
-	jwt.Signature = jwt.getSignature()
+func (jwt *Jwt) Encode() string {
+	header, _ := json.Marshal(jwt.Header)
+	payload, _ := json.Marshal(jwt.Payload)
+	return base64UrlEncode(string(header)) + "." + base64UrlEncode(string(payload)) + "." + jwt.Signature
+}
+func Decode(jwt_str string) (*Jwt, error) {
+	jwt_splits := strings.Split(jwt_str, ".")
+	if len(jwt_splits) != 3 {
+		return nil, errors.New("Invalid auth code")
+	}
+
+	_header, err := base64UrlDecode(jwt_splits[0])
+	if err != nil {
+		return nil, errors.New("Invalid auth code")
+	}
+	jwt_header := &JwtHeader{}
+	err = json.Unmarshal([]byte(_header), jwt_header)
+	if err != nil {
+		return nil, errors.New("Invalid auth code")
+	}
+
+	_payload, err := base64UrlDecode(jwt_splits[1])
+	if err != nil {
+		return nil, errors.New("Invalid auth code")
+	}
+	jwt_payload := &JwtPayload{}
+	err = json.Unmarshal([]byte(_payload), jwt_payload)
+	if err != nil {
+		return nil, errors.New("Invalid auth code")
+	}
+
+	_signature := jwt_splits[2]
+
+	return &Jwt{Header: *jwt_header, Payload: *jwt_payload, Signature: _signature}, nil
 }
 
-func (jwt *Jwt) getSignature() string {
+func (jwt *Jwt) setSignature() error {
+	s, err := jwt.calcSignature()
+	if err == nil {
+		jwt.Signature = s
+	}
+	return err
+}
 
+func (jwt *Jwt) calcSignature() (string, error) {
+	header, err := json.Marshal(jwt.Header)
+	if err != nil {
+		return "", err
+	}
+	payload, err := json.Marshal(jwt.Payload)
+	if err != nil {
+		return "", err
+	}
+	in := string(header) + "." + string(payload)
+	h := hash(in)
+	return base64UrlEncode(h), nil
+}
+
+func hash(s string) string {
+	b := sha256.Sum256([]byte(s))
+	return string(b[:])
 }
 
 func base64UrlEncode(s string) string {
+	return base64.RawURLEncoding.EncodeToString([]byte(s))
+}
 
+func base64UrlDecode(s string) (string, error) {
+	b, err := base64.RawURLEncoding.DecodeString(s)
+	if err != nil {
+		return "", err
+	}
+	return string(b), err
 }
 
 func (jwt *Jwt) Authenticate() (bool, error) {
-
+	sig, err := jwt.calcSignature()
+	if err == nil {
+		// If Signature is valid and timestamp is not expired then true.
+		if jwt.Signature == sig && jwt.Payload.ExpireAt.After(time.Now()) {
+			return true, nil
+		}
+	}
+	return false, nil
 }
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/jwt/jwt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"net/http"
@@ -11,16 +12,35 @@ func main() {
 	fmt.Println("start")
 	e := echo.New()
 	e.Use(middleware.Logger())
-	e.GET("/mypage", MyPageHandler)
+
 	e.GET("/login", LoginHandler)
+	require_auth_group := e.Group("", AuthenticateMiddleware())
+	require_auth_group.GET("/mypage", MyPageHandler)
 	e.Start(":9999")
+}
+
+func AuthenticateMiddleware() echo.MiddlewareFunc {
+	return func(handler echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// Authentication
+			if _jwt, err := jwt.Decode(c.QueryParam("auth")); err == nil {
+				if ok, _ := _jwt.Authenticate(); ok {
+					// When authentication is valid
+					return handler(c)
+				}
+			}
+			// return echo.NewHTTPError(http.StatusUnauthorized)
+			return c.Redirect(http.StatusUnauthorized, "/login")
+		}
+	}
 }
 
 func LoginHandler(c echo.Context) error {
 	if c.QueryParam("user_id") == "2" {
-		return c.JSONPretty(http.StatusOK, map[string]string{"user_id": "1", "expire_at": "2019-05-01"}, "\t")
+		j, _ := jwt.Publish(map[string]string{"user_id": c.QueryParam("user_id")})
+		return c.String(http.StatusOK, j.Encode())
 	} else {
-		return c.JSONPretty(http.StatusOK, nil, "\t")
+		return c.String(http.StatusOK, "Login Failed")
 	}
 }
 
